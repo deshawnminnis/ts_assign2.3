fa25111397d19af7d568c099261d358fcef206e4
De'Shawn Minnis <deshawnminnis@gmail.com>
2017-03-23 11:37:16 -0700

    Add files via upload

---
+++ as_2.3.js
@@ -0,0 +1,25 @@
+// Typescript: Example of Class creation with get and set function //
+var myContacts = (function () {
+    function myContacts() {
+    }
+    Object.defineProperty(myContacts.prototype, "contactInfo", {
+        // check to see if contact name is valid when returning value //
+        get: function () {
+            if (name === undefined) {
+                return this.name;
+            }
+            else {
+                alert("This user is not valid");
+            }
+        },
+        set: function (name) {
+            this.name = name;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return myContacts;
+}());
+var contact = new myContacts();
+contact.contactInfo = "Jennifer Minnis";
+alert(contact.contactInfo);
---
+++ as_2.3.ts
@@ -0,0 +1,26 @@
+// Typescript: Example of Class creation with get and set function //
+
+class myContacts { 
+    private name: string = null; 
+    private phone: number = null; 
+
+// check to see if contact name is valid when returning value //
+    get contactInfo(): string {
+        if (name === undefined) {
+            return this.name;
+        }else {
+            alert("This user is not valid");
+        }
+
+    }
+    
+    set contactInfo(contactName: string) {
+        this.name = contactName;
+    
+    }
+
+}
+
+var contact = new myContacts();
+contact.contactInfo = "Jennifer Minnis";
+alert(contact.contactInfo);
